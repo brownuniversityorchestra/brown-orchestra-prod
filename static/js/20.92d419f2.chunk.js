2264e1e872f389a8afff0343461ffc0fba7c559b
HyperKids <HyperKids@users.noreply.github.com>
2022-12-12 16:14:48 +0000

    Update main to output generated at 7d452e3

--- static/js/20.92d419f2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbrown-edu-orchestra"]=this["webpackJsonpbrown-edu-orchestra"]||[]).push([[20],{96:function(e){e.exports=JSON.parse('{"year":"2022","videos":[{"date":"2022-05-07T00:00:00.000Z","title":"*Finlandia* by Jean Sibelius","link":"https://www.youtube.com/embed/V7afF7Ix-Ns&ab_channel=BrownUniversityDepartmentofMusic","location":"Sayles Hall"}]}')}}]);
-//# sourceMappingURL=20.92d419f2.chunk.js.map
