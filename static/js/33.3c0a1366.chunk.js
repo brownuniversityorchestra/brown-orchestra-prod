b18554ac961bdf5a6f7e2757a27038ab397f61e0
HyperKids <HyperKids@users.noreply.github.com>
2024-09-22 21:09:47 +0000

    Update main to output generated at 4e658cf

---
+++ static/js/33.3c0a1366.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbrown-edu-orchestra"]=this["webpackJsonpbrown-edu-orchestra"]||[]).push([[33],{111:function(e){e.exports=JSON.parse('{"year":"2023","videos":[{"date":"2023-10-21T00:00:00.000Z","link":"https://www.youtube.com/embed/-BP_Jw1xNh0","title":"Open Again a Turn of Light by Eric Nathan","location":"Lindemann Performing Arts Center"}]}')}}]);
+//# sourceMappingURL=33.3c0a1366.chunk.js.map
